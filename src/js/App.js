3c378c07537af54053873f16c859c1d7308b9f92
immanoj16 <mmaharana14@gmail.com>
2017-08-06 16:58:50 +0530

    Add a state named isExpanded to see the transition

--- src/js/App.js
+++ src/js/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
                 <div className="content">
                     <div className="panel-group">
                         <Collapsible title="Overview">
-                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, maxime. Repellat nemo rerum inventore ut quam laboriosam minima nisi id temporibus, veniam perferendis facilis placeat, asperiores alias cum eius aut facere atque quasi. Adipisci voluptates ad praesentium fugit cum ex, exercitationem qui, voluptate, quisquam accusamus consequuntur aliquam nobis voluptatibus esse. Tempora necessitatibus nesciunt repellendus doloremque ducimus error maxime distinctio, modi. Harum quibusdam eligendi molestias sequi mollitia facere, autem explicabo similique ab modi repellat iste reiciendis natus cupiditate suscipit dolorem. Odio possimus excepturi quam deleniti quibusdam, repudiandae dignissimos vel rerum laborum quisquam quas minus hic voluptatem dolor corporis a accusamus necessitatibus!</p>
                         </Collapsible>
                         <Collapsible title="Features">
                             <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -5,17 +5,31 @@ class Collapsible extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			isExpanded: false
+		}
+	}
+
+	handleToggle(e) {
+		e.preventDefault();
+		console.log(this.refs.inner.clientHeight);
+		this.setState({
+			isExpanded: !this.state.isExpanded
+		})
 	}
 
 	render() {
 		const {title, children} = this.props;
+		const {isExpanded} = this.state;
 		return (
-			<div className="panel">
+			// Back ticks (``)
+			<div className={`panel ${isExpanded ? 'is-expanded' : ''}`} onClick={(e) => this.handleToggle(e)}>
 					<div className="panel-heading">
 							<h2>{title}</h2>
 					</div>
 					<div className="panel-collapse">
-							<div className="panel-body">
+							<div className="panel-body" ref="inner">
 									{children}
 							</div>
 					</div>
