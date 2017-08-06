d2e3628d64b28a25324a8554d3e4957d848710c1
immanoj16 <mmaharana14@gmail.com>
2017-08-06 16:03:10 +0530

    Add: all collapsible code to Collapsible.js file

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../images/expand-vertical-4.svg';
+import Collapsible from './Collapsible';
 
 class App extends React.Component {
     render() {
@@ -14,16 +15,12 @@ class App extends React.Component {
                         <Collapsible title="Overview">
                             <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
                         </Collapsible>
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Reviews</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
+                        <Collapsible title="Features">
+                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
+                        </Collapsible>
+                        <Collapsible title="Software">
+                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
+                        </Collapsible>
                     </div>
                 </div>
             </div>
