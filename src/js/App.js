2b6e7c93f85d631cecb84766b51e28f669198600
immanoj16 <mmaharana14@gmail.com>
2017-08-06 15:33:51 +0530

    First commit of Collapsible

---
+++ src/js/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import image from '../images/expand-vertical-4.svg';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <img src={image} />
+                    <h1>Collapsible Content</h1>
+                </header>
+                <div className="content">
+                    <div className="panel-group">
+                        <Collapsible title="Overview">
+                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
+                        </Collapsible>
+                        <div className="panel">
+                            <div className="panel-heading">
+                                <h2>Reviews</h2>
+                            </div>
+                            <div className="panel-collapse">
+                                <div className="panel-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
